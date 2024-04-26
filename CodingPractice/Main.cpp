74bd224ece237405443ccb349fccaaf04b24109a
Richard Horton <richardhorton1312@gmail.com>
2024-04-26 15:51:13 +0100

    Set challenge parameters and bonus objectives

--- CodingPractice/Main.cpp
+++ CodingPractice/Main.cpp
@@ -1,3 +1,17 @@
+/*
+* Get birth year from the user
+* Calculate age from birth year
+* Convert years to days
+* Return number of days for age
+*
+* Extra challenges:
+* Include extra days for leap years
+* Calculate using birth day to current date
+* Check for only positive integer inputs
+* Check for outputs
+* Handle input related exceptions
+*/
+
 #include <iostream>
 
 int main() {
